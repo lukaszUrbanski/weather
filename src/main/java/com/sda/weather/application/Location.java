d69361a7c8091eead0ba5714e075fe4a1ef9de84
Lukasz Urbanski <lukasz.urbanski94@gmail.com>
2020-10-17 14:38:48 +0200

    second commit

--- src/main/java/com/sda/weather/application/Location.java
+++ src/main/java/com/sda/weather/application/Location.java
@@ -4,6 +4,8 @@ public class Location {
 
     private String name;
     private String coordinate;
+    private String region;
+    private String country;
 
 
     public Location(final String name, final String coordinate, final String region, final String country) {
